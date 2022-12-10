b3d264a6487377f12263e1477fe3247b0d2f5a4b
Mark Calcagno <mcalcagn@gmail.com>
2022-12-09 20:17:45 -0600

    I don't know why I can't see the notes...

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use('/api', api);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
