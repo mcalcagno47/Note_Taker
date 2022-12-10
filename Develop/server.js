39c7898fea9747208cb61e2d60bd73b0f183689a
Mark Calcagno <mcalcagn@gmail.com>
2022-12-09 20:33:47 -0600

    added id js

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const uuid = require('./public/assets/js/uuid')
 
 const PORT = process.env.PORT || 3001;
 
@@ -66,6 +67,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             noteText,
+            note_id: uuid(),
         };
 
         readAndAppend(newNote, './db/db.json');
